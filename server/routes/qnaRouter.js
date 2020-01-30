21ec126e122c2c49901cec50e48e4b35601fa90b
ninoloid <muhammadsatriaadiputra@gmail.com>
2020-01-30 20:39:23 +0700

    api documentation added

--- server/routes/qnaRouter.js
+++ server/routes/qnaRouter.js
@@ -7,14 +7,12 @@ const authorization = require('../middlewares/authorization')
 
 router.get('/', postController.showAllQuestion)
 router.get('/:id', postController.showOneQuestion)
-router.delete('/:id', postController.deleteQuestion)
+router.delete('/:id', authentication, authorization, postController.deleteQuestion)
 router.post('/wopic', authentication, postController.addQuestionWithoutPic)
 router
   .route('/')
   .post(multer({ dest: 'temp/', limits: { fieldSize: 3 * 1024 * 1024 } }).single('imageUrl'), authentication, postController.addQuestion)
 router.post('/answer', authentication, postController.addAnswer)
 router.post('/vote', authentication, postController.addVote)
-router.delete('/:id', authentication, authorization, postController.deletePost)
-
 
 module.exports = router
